37fb4196d4c2df8ece1499203d2d7544cbc47a79
Nicola Tonellotto <nicola.tonellotto@gmail.com>
2014-11-11 13:46:07 +0100

    gitignore added

--- src/main/java/it/cnr/isti/hpclab/succinct/structures/SuccinctBasicIterablePosting.java
+++ src/main/java/it/cnr/isti/hpclab/succinct/structures/SuccinctBasicIterablePosting.java
@@ -5,16 +5,11 @@ import it.cnr.isti.hpclab.succinct.util.FreqReader;
 import it.cnr.isti.hpclab.succinct.util.LongWordBitReader;
 import it.cnr.isti.hpclab.succinct.util.Utils;
 import it.unimi.dsi.fastutil.longs.LongBigList;
-import it.unimi.dsi.util.ByteBufferLongBigList;
+// import it.unimi.dsi.util.ByteBufferLongBigList;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.ByteOrder;
-import java.nio.channels.FileChannel.MapMode;
 
 import org.terrier.structures.DocumentIndex;
-import org.terrier.structures.Index;
 import org.terrier.structures.postings.IterablePosting;
 import org.terrier.structures.postings.WritablePosting;
 
